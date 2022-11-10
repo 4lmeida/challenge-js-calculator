310739aed12ac241470063ec2208f3ced274269b
Luis Almeida <lferreira646@gmail.com>
2022-11-10 10:36:45 -0300

    Update

--- js/main.js
+++ js/main.js
@@ -1,47 +1,90 @@
 const inputNumber1 = document.getElementById("number1");
-inputNumber1.addEventListener('keyup', handleImputNumberKeyup1);
+inputNumber1.addEventListener('keyup', handleInputNumber1Keyup);
 
 const inputNumber2 = document.getElementById("number2");
-inputNumber2.addEventListener('keyup', handleImputNumberKeyup2);
+inputNumber2.addEventListener('keyup', handleInputNumber2Keyup);
 
 const btnPlus = document.getElementById("btn-plus");
 btnPlus.addEventListener("click", handleBtnPlusOnclick);
 
 const btnTimes = document.getElementById("btn-times");
-
+btnTimes.addEventListener('click', handleBtnTimesClick);
 
 const btnClear = document.getElementById("btn-clear");
+btnClear.addEventListener('click', handleBtnClearClick);
 
 const resulBox = document.getElementById('result-box');
 
-function handleImputNumberKeyup1(event) {
-    resulBox.innerHTML =  Number(event.target.value);
-}
-
-function handleImputNumberKeyup2(event) { 
-    resulBox.innerHTML = Number(event.target.value);
-}
-
-console.log(resulBox.innerHTML)
 
 
-function handleBtnPlusOnclick() {
-    resulBox.innerHTML = 8 + 8;
+function handleBtnTimesClick() {
+    const n1 = inputNumber1.value;
+    const n2 = inputNumber2.value;
 
-}
+    console.log(inputNumber1.value);
+    console.log(inputNumber2.value);
 
-console.log(resulBox.innerHTML);
-
-/*function result(event1, event2) {
-    if(btnPlus === "+") {
-        return event1.handleImputNameKeyup1 + event2.handleImputNameKeyup2;
-    }
-    else if(btnTimes === "x") {
-        return event1.handleImputNameKeyup1 * event2.handleImputNameKeyup2;
+    if (isNumber(n1) && isNumber(n2)) {
+        resulBox.innerHTML = Number(n1) * Number(n2);
+        inputNumber1.classList.remove("input-error")
+        inputNumber2.classList.remove("input-error")
     }
     else {
-        return 0;
+        inputNumber1.classList.add("input-error")
+        inputNumber2.classList.add("input-error")
+    }
+
+    
+}
+
+function handleBtnPlusOnclick() {
+    const n1 = inputNumber1.value;
+    const n2 = inputNumber2.value;
+
+    console.log(inputNumber1.value);
+    console.log(inputNumber2.value);
+
+    if (isNumber(n1) && isNumber(n2)) {
+        resulBox.innerHTML = Number(n1) + Number(n2);
+        inputNumber1.classList.remove("input-error")
+        inputNumber2.classList.remove("input-error")
+    }
+    else {
+        inputNumber1.classList.add("input-error")
+        inputNumber2.classList.add("input-error")
+    }
+   
+}
+
+function handleBtnClearClick() {
+    inputNumber1.value = "";
+    inputNumber2.value = "";
+    resulBox.innerHTML = 0;
+    inputNumber1.classList.remove("input-error")
+    inputNumber2.classList.remove("input-error")
+
+    inputNumber1.focus();
+
+}
+
+function handleInputNumber1Keyup(event) {
+    if(isNumber(event.target.value)) {
+        event.target.classList.remove("input-error")
+    }
+    else {
+        event.target.classList.add("input-error")
     }
 }
 
-console.log(result.value);*/
+function handleInputNumber2Keyup(event) {
+    if(isNumber(event.target.value)) {
+        event.target.classList.remove("input-error")
+    }
+    else {
+        event.target.classList.add("input-error")
+    }
+}
+
+function isNumber(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
