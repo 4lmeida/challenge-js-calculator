fec006fc1550776a4dc71742a568d8d6b6b1b64d
Luis Almeida <lferreira646@gmail.com>
2022-11-10 12:04:02 -0300

    Update end

--- js/main.js
+++ js/main.js
@@ -1,59 +1,39 @@
-const inputNumber1 = document.getElementById("number1");
-inputNumber1.addEventListener('keyup', handleInputNumber1Keyup);
+const inputNumber1 = document.getElementById('number1');
+inputNumber1.addEventListener('keypress', handleKeyPress);
 
-const inputNumber2 = document.getElementById("number2");
-inputNumber2.addEventListener('keyup', handleInputNumber2Keyup);
+const inputNumber2 = document.getElementById('number2');
+inputNumber2.addEventListener('keypress', handleKeyPress);
 
-const btnPlus = document.getElementById("btn-plus");
+const btnPlus = document.getElementById('btn-plus');
 btnPlus.addEventListener("click", handleBtnPlusOnclick);
 
-const btnTimes = document.getElementById("btn-times");
+const btnTimes = document.getElementById('btn-times');
 btnTimes.addEventListener('click', handleBtnTimesClick);
 
-const btnClear = document.getElementById("btn-clear");
+const btnClear = document.getElementById('btn-clear');
 btnClear.addEventListener('click', handleBtnClearClick);
 
 const resulBox = document.getElementById('result-box');
 
+const inputError = document.querySelector('[class="form-input input-error"]');
+inputError.classList.remove('input-error');
+
 
 
 function handleBtnTimesClick() {
-    const n1 = inputNumber1.value;
-    const n2 = inputNumber2.value;
+    inputErrors();
 
-    console.log(inputNumber1.value);
-    console.log(inputNumber2.value);
-
-    if (isNumber(n1) && isNumber(n2)) {
-        resulBox.innerHTML = Number(n1) * Number(n2);
-        inputNumber1.classList.remove("input-error")
-        inputNumber2.classList.remove("input-error")
-    }
-    else {
-        inputNumber1.classList.add("input-error")
-        inputNumber2.classList.add("input-error")
-    }
-
-    
+    if (isNumber(inputNumber1.value) && isNumber(inputNumber2.value)) {
+        resulBox.innerHTML = Number(inputNumber1.value) * Number(inputNumber2.value);
+    } 
 }
 
 function handleBtnPlusOnclick() {
-    const n1 = inputNumber1.value;
-    const n2 = inputNumber2.value;
+    inputErrors();
 
-    console.log(inputNumber1.value);
-    console.log(inputNumber2.value);
-
-    if (isNumber(n1) && isNumber(n2)) {
-        resulBox.innerHTML = Number(n1) + Number(n2);
-        inputNumber1.classList.remove("input-error")
-        inputNumber2.classList.remove("input-error")
+    if (isNumber(inputNumber1.value) && isNumber(inputNumber2.value)) {
+        resulBox.innerHTML = Number(inputNumber1.value) + Number(inputNumber2.value);
     }
-    else {
-        inputNumber1.classList.add("input-error")
-        inputNumber2.classList.add("input-error")
-    }
-   
 }
 
 function handleBtnClearClick() {
@@ -67,24 +47,32 @@ function handleBtnClearClick() {
 
 }
 
-function handleInputNumber1Keyup(event) {
-    if(isNumber(event.target.value)) {
-        event.target.classList.remove("input-error")
-    }
-    else {
-        event.target.classList.add("input-error")
-    }
-}
-
-function handleInputNumber2Keyup(event) {
-    if(isNumber(event.target.value)) {
-        event.target.classList.remove("input-error")
-    }
-    else {
-        event.target.classList.add("input-error")
+function handleKeyPress(event) {
+    let theEvent = event;
+    let key = event.keyCode || theEvent.which;
+    key = String.fromCharCode(key);
+    let regex = /^-?\d*\.?\d*$/;
+    if(!regex.test(key)) {
+        theEvent.returnValue = false;
+        if(theEvent.preventDefault) theEvent.preventDefault();
     }
 }
 
 function isNumber(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function inputErrors() {
+    if(!isNumber(inputNumber1.value)) {
+        inputNumber1.classList.add("input-error")
+    }
+    else {
+        inputNumber1.classList.remove("input-error")
+    }
+    if(!isNumber(inputNumber2.value)) {
+        inputNumber2.classList.add("input-error")
+    }
+    else {
+        inputNumber2.classList.remove("input-error")
+    }
 }
